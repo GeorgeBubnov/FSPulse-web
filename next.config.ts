597c94d4c2866751dc21ec17c9f361691926793f
George <gopractic@gmail.com>
2025-04-24 01:37:37 +0300

    fix: maybe work prisma vercel 4!

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
+const nextConfig: NextConfig = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            // eslint-disable-next-line
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        // eslint-disable-next-line
+        return config;
+    },
+};
 
 export default nextConfig;
